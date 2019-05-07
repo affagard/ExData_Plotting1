8760e69623751606381056821566564a2cd19d3c
David Affagard <david@affagard.com>
2019-05-07 19:57:19 +0200

    gitignore data_source

--- plot1.R
+++ plot1.R
@@ -1,5 +1,26 @@
 ## Load data source file
-# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# Dataset: Electric power consumption [20Mb]
+#
+file_name <- "data_source/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+#
+#
+# Create local folder data_source
+if (!dir.exists("data_source")){
+        dir.create("data_source", showWarnings = TRUE, recursive = FALSE, mode = "0744")
+}
+#
+#
+# Download file_name if needed
+if (!file.exists(file_name)){
+        print("Waiting while dowloading zipped data source ...")
+        download.file(file_url, file_name, method="curl")
+}
+#
+#
+if (!file.exists("data_source/household_power_consumption.txt")) { 
+        unzip(file_name, exdir = "data_source") 
+}
 
 ## Unzip file
 
