727d937c4d509743fe6ff1ef499420af6f78f26c
David Affagard <david@affagard.com>
2019-05-07 19:19:33 +0200

    Initial Commit for plot1.R and RStudio file

---
+++ plot1.R
@@ -0,0 +1,9 @@
+## Load data source file
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+
+## Unzip file
+
+## Read data over a 2-day period in February, 2007
+#  (dates 2007-02-01 and 2007-02-02)
+
+## Plot 1 : Global Active Power
